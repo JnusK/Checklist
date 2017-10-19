90c1d3328fa5e3e00669870859b70761a40b9ce5
natrecneps <spencertan@hotmail.sg>
2017-10-20 01:21:09 +0800

    Merge branch 'master' of https://github.com/JnusK/Checklist
    # Conflicts:
    #	.idea/workspace.xml

---
+++ app/src/main/java/highlighter/checklistapp/boundary/AdminCreateAccount.java
@@ -0,0 +1,16 @@
+package highlighter.checklistapp.boundary;
+
+import android.os.Bundle;
+
+import highlighter.checklistapp.R;
+
+/**
+ * Created by Spencer on 20/10/2017.
+ */
+
+public class AdminCreateAccount extends AdminHomepage {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_createaccount);
+    }
+}
--- app/src/main/java/highlighter/checklistapp/boundary/AdminHomepage.java
+++ app/src/main/java/highlighter/checklistapp/boundary/AdminHomepage.java
@@ -1,12 +1,12 @@
 package highlighter.checklistapp.boundary;
 
-import highlighter.checklistapp.LoginActivity;
-import highlighter.checklistapp.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.view.View.OnClickListener;
+
+import highlighter.checklistapp.LoginActivity;
+import highlighter.checklistapp.R;
 
 public class AdminHomepage extends LoginActivity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +22,23 @@ public class AdminHomepage extends LoginActivity implements OnClickListener {
 
             //To be updated, Spencer 19/10/2017
             case R.id.admin_homepage_textViewcreate:
-                i = new Intent(this, UserHomepage.class);
+                i = new Intent(AdminHomepage.this, AdminCreateAccount.class);
                 startActivity(i);
                 break;
             case R.id.admin_homepage_textViewedit:
-                i = new Intent(this, UserHomepage.class);
+                i = new Intent(AdminHomepage.this, UserHomepage.class);
                 startActivity(i);
                 break;
             case R.id.admin_homepage_textViewmodify:
-                i = new Intent(this, UserHomepage.class);
+                i = new Intent(AdminHomepage.this, UserHomepage.class);
                 startActivity(i);
                 break;
             case R.id.admin_homepage_textViewstatus:
-                i = new Intent(this, UserHomepage.class);
+                i = new Intent(AdminHomepage.this, UserHomepage.class);
                 startActivity(i);
                 break;
             case R.id.admin_homepage_textViewchecklist:
-                i = new Intent(this, UserHomepage.class);
+                i = new Intent(AdminHomepage.this, UserHomepage.class);
                 startActivity(i);
                 break;
         }

7983f544792a5d39e4b28ae849fc956183e8be61
natrecneps <spencertan@hotmail.sg>
2017-10-20 21:02:06 +0800

    Merge branch 'master' of https://github.com/JnusK/Checklist
    # Conflicts:
    #	.idea/workspace.xml
    
    User front end done

--- app/src/main/java/highlighter/checklistapp/boundary/CustomListAdapter2.java
+++ app/src/main/java/highlighter/checklistapp/boundary/CustomListAdapter2.java
@@ -1,8 +1,37 @@
 package highlighter.checklistapp.boundary;
 
-/**
- * Created by Spencer on 20/10/2017.
- */
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class CustomListAdapter2 {
+import highlighter.checklistapp.R;
+
+public class CustomListAdapter2 extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] itemname;
+
+    public CustomListAdapter2(Activity context, String[] itemname) {
+        super(context, R.layout.mylist2, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.itemname=itemname;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.mylist2, null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.textViewChecklistName);
+        TextView extratxt = (TextView) rowView.findViewById(R.id.textViewChecklistDescription);
+
+        txtTitle.setText(itemname[position]);
+        extratxt.setText("Description "+itemname[position]);
+        return rowView;
+
+    };
 }
--- app/src/main/java/highlighter/checklistapp/boundary/UserDetailedChecklistPage.java
+++ app/src/main/java/highlighter/checklistapp/boundary/UserDetailedChecklistPage.java
@@ -2,6 +2,8 @@ package highlighter.checklistapp.boundary;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import highlighter.checklistapp.R;
@@ -9,6 +11,13 @@ import highlighter.checklistapp.R;
 public class UserDetailedChecklistPage extends UserChecklistsPage{
 
     TextView textViewChecklistItem;
+    CheckBox cbItem;
+
+    ListView list;
+    String[] itemname ={
+            "Test Building",
+            "Test Building 2",
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -17,6 +26,26 @@ public class UserDetailedChecklistPage extends UserChecklistsPage{
 
         textViewChecklistItem = (TextView)findViewById(R.id.textViewChecklistItem);
         getChecklistDescription();
+
+        CustomListAdapter2 adapter = new CustomListAdapter2(this, itemname);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        /*
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                //For testing purposes
+                Toast.makeText(getApplicationContext(), "End", Toast.LENGTH_SHORT).show();
+            }
+        });
+        */
+
+         cbItem = (CheckBox) (CheckBox)findViewById(R.id.cbChecklistItem);
+
     }
 
     private void getChecklistDescription(){

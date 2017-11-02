fe936f566d9197bd539c419f0dc2d4968f12df02
Janus <jnus@live.com.sg>
2017-11-02 23:52:36 +0800

    DAO

--- app/src/main/java/highlighter/checklistapp/UserDAO.java
+++ app/src/main/java/highlighter/checklistapp/UserDAO.java
@@ -4,18 +4,19 @@ import android.content.Context;
 import android.util.Log;
 
 import highlighter.checklistapp.controller.AccessUserDB;
+import highlighter.checklistapp.entity.UserDB;
 
 /**
  * Created by Khorly on 2/11/17.
  */
 
 public class UserDAO {
-    public static AccessUserDB accessUserDB;
+    public static UserDB accessUserDB;
 
     public static void DBHandler(Context context){
         Log.d("DBHandler", "user ");
 
-        accessUserDB = new AccessUserDB(context); // This will be your DB Handler Class
+        accessUserDB = new UserDB(context); // This will be your DB Handler Class
         accessUserDB.addUserToDB(12345, "admin", 1, 0);
         accessUserDB.addUserToDB(234, "tech", 0, 0);
     }

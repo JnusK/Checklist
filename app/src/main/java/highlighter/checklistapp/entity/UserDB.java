95f2b166d9177dd48eb1edbf07e318557701273b
Janus <jnus@live.com.sg>
2017-11-02 21:01:05 +0800

    Update UserDB.java

--- app/src/main/java/highlighter/checklistapp/entity/UserDB.java
+++ app/src/main/java/highlighter/checklistapp/entity/UserDB.java
@@ -1,49 +1,224 @@
 package highlighter.checklistapp.entity;
 
+import android.content.ContentValues;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.view.View;
+import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.json.JSONStringer;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Set;
 
-import static android.app.PendingIntent.getActivity;
+import highlighter.checklistapp.controller.AccessUserDB;
 
 /**
- * Created by Joseph on 10/24/2017.
+ * Created by Jnus on 23/10/2017.
  */
 
-public class UserDB {
+public class UserDB extends SQLiteOpenHelper {
 
-    ArrayList<User> userDB;
+    // Database Version
+    private static final int DATABASE_VERSION = 5;
+    // Database Name
+    private static final String DATABASE_NAME = "UserDB";
+    // Contacts table name
+    private static final String TABLE_USERS = "Users";
+    // Shops Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_TYPE = "type";
+    private static final String KEY_DATE_ADDED = "date_added";
 
-    public void addUser(User user){
+    ArrayList<User> userArray = new ArrayList<User>();
+
+    public UserDB(Context context) {
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        userArray = getAllUsers();
+
+    }
+
+    //For debugging to check if the database exist
+    public void checkDB(){
+        try{
+            SQLiteDatabase db = this.getWritableDatabase();
+            db.close();
+            Log.w("userDB", "DB exist");
+        }catch (Exception e) {
+
+            Log.i(getClass().getName(), e.toString());
+            System.out.print(e);
+            Log.w("userDB","DB does not exist");
+        }
+    }
+
+
+
+    public int findUser(int user_id){
         /**
-         * Add a user to the ArrayList<User>
+         * Used to check if user exist in DB.
+         * Return the index when exist and -1 when does not exist
          */
-        this.userDB.add(user);
+        for (int x=0; x<userArray.size(); x++) {
+            if (userArray.get(x).getID()==(user_id)) {
+                Log.w("userDB", "user exist");
+                return x;
+            }
+        }
+        Log.w("userDB", "user does not exist");
+        return -1;
     }
 
-    public ArrayList<User> getUsers(){
-        return userDB;
+    public int checkPassword(String password, int index){
+        /**
+         * Check if pw provided by user tallies with DB
+         * Return 1 when correct and 0 when incorrect
+         */
+        if(userArray.get(index).getPassword().equals(password)){
+            Log.w("userDB", "pw correct");
+            return 1;
+        }
+        Log.w("userDB", "pw not correct");
+        return 0;
+
     }
 
-    public UserDB(ArrayList<User> userDB){
-        this.userDB = userDB;
+    public int authenticateUser(int id, String password){
+        /**
+         * Check user and password against user database
+         * return 1 if successful
+         * return 0 if user does not exist
+         * return -1 if password is incorrect
+         */
+        int index;
+        index = this.findUser(id);
+        if(index == -1){
+            return 0;
+        }
+        //password is wrong
+        if(this.checkPassword(password, index) == 0){
+            return -1;
+        }
+        //successful authentication
+        else {
+            Log.w("userDB", "user authenticated");
+            return 1;
+        }
     }
 
 
+    public int checkUserType(int user_id){
+        /**
+         * Check if user is admin or user
+         * 1 is admin, 0 is user, -1 is invalid
+         */
+        for (int x=0; x<userArray.size(); x++) {
+            if (userArray.get(x).getID()==(user_id)) {
+                Log.w("userDB", "usertype verified");
+                return userArray.get(x).getType();
+            }
+        }
+        return -1;
+    }
+
+
+    public int addUserToDB(int id, String password, int type, int date_added){
+        /**
+         * Add account to DB
+         * return 1 if successful and 0 if user exist
+         */
+        if(this.findUser(id) == 1){
+            return 0;
+        }
+
+        User user = new User(id, password, type, date_added);
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, user.getID()); // user_id
+        values.put(KEY_PASSWORD, user.getPassword()); // Password
+        values.put(KEY_TYPE, user.getType());
+        values.put(KEY_DATE_ADDED, user.getDateAdded());
+
+        // Inserting Row into SQLite
+        db.insert(TABLE_USERS, null, values);
+        db.close(); // Closing database connection
+        //Add into object
+        userArray.add(user);
+        Log.w("userDB", "User added");
+        return 1;
+    }
+
+    public int changePassword(int user_id, String new_pw){
+        /**
+         * Allow user to change pw
+         * 1 is successful, 0 is unsuccessful
+         */
+        for (int x=0; x<userArray.size(); x++) {
+            if (userArray.get(x).getID()==(user_id)) {
+                //Update object
+                userArray.get(x).setPassword(new_pw);
+                //Update DB
+                int reply = updateUserDB(userArray.get(x));
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public int updateUserDB(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, user.getID());
+        values.put(KEY_PASSWORD, user.getPassword());
+        values.put(KEY_TYPE, user.getType());
+        values.put(KEY_DATE_ADDED, user.getDateAdded());
+// updating row
+        return db.update(TABLE_USERS, values, KEY_ID + " = ?",
+                new String[]{String.valueOf(user.getID())});
+    }
+
+    // Getting All Users
+    public ArrayList<User> getAllUsers() {
+        ArrayList<User> userList = new ArrayList<User>();
+        // Select All Query
+        String selectQuery = "SELECT * FROM " + TABLE_USERS;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                User user = new User(0, "", 0, 0);
+                user.setID(cursor.getInt(0));
+                user.setPassword(cursor.getString(1));
+                user.setType(cursor.getInt(2));
+                user.setDateAdded(cursor.getInt(3));
+        // Adding user to list
+                userList.add(user);
+            } while (cursor.moveToNext());
+        }
+    // return user list
+        Log.w("userDB", "userArray populated");
+        return userList;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_USERS + "("
+        + KEY_ID + " INTEGER PRIMARY KEY," + KEY_PASSWORD + " TEXT,"
+        + KEY_TYPE + " INTEGER," + KEY_DATE_ADDED + " INTEGER" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+        Log.w("userDB", "DB created");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        Log.w("userDB", "DB updated");
+        // Creating tables again
+        onCreate(db);
+    }
 }

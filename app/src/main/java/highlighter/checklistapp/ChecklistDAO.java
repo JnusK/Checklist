d1b587aa8406ba693dd7e68327c2362bb4e9d3ac
Janus <jnus@live.com.sg>
2017-11-07 22:53:32 +0800

    Populate with proper data

--- app/src/main/java/highlighter/checklistapp/ChecklistDAO.java
+++ app/src/main/java/highlighter/checklistapp/ChecklistDAO.java
@@ -19,7 +19,7 @@ public class ChecklistDAO {
 
     public static void DBHandler(Context context){
         Log.d("DBHandler", "user ");
-        
+
         accessChecklistDB = new ChecklistDB(context);
         populateChecklist();
 
@@ -28,23 +28,26 @@ public class ChecklistDAO {
 
     public static void populateChecklist() {
         if (accessChecklistDB.getAllCheckListItemFromArchive().size()==0) {
-            //Checking to prevent duplication of items and checklist when doing initial population
-            accessChecklistDB.addChecklistToDB("Daily item 1", "Daily", ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(0,"Daily item 1 description1" , 0 , ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(0,"Daily item 1 description2" , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addChecklistToDB("Unloading Rack", "Daily", ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(0,"Inspect all equipment installed for any signs of leaks, cracks, chipped paint, corrosion and missing parts. Ensure all manifolds are covered with protective caps." , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(0,"Inspect the hose for signs of external damage and ensure protective caps are in place." , 0 , ChecklistDB.ARCHIVE);
 
-            accessChecklistDB.addChecklistToDB("Daily item 2", "Daily", ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(1,"Daily item 2 description1" , 0 , ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(1,"Daily item 2 description2" , 0 , ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(1,"Daily item 2 description3" , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addChecklistToDB("Slop Tank", "Daily", ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(1,"Visually check all equipment installed in slop tank site and pits for signs of leaks, damage or missing parts." , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(1,"Operate hand pump to draw samples from tank sump and carry out WPT." , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(1,"Manually drain basket strainer." , 0 , ChecklistDB.ARCHIVE);
 
-            accessChecklistDB.addChecklistToDB("Weekly item 2", "Weekly", ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(2,"Weekly item 2 description1" , 0 , ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(2,"Weekly item 2 description2" , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addChecklistToDB("Control Building", "Weekly", ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(2,"Switch on and check all lightings, air-conditioners and emergency lightings are serviceable." , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(2,"Check that loading pump panel switches are mechanically serviceable by operating on and off." , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(2,"Check that unloading pump panel switches are mechanically serviceable by operating on and off." , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(2,"Check fire phone is serviceable by picking up the hand set and getting response from the fire station." , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(2,"Visually check the DC and facility panel switches are serviceable. (all the light bulbs should glow)" , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(2,"Test & check that PA / intercom systems are serviceable. (If applicable)" , 0 , ChecklistDB.ARCHIVE);
+            accessChecklistDB.addCheckListItemsToDB(2,"Monitor and ensure all equipment are calibrated by means of record or white board display." , 0 , ChecklistDB.ARCHIVE);
 
             accessChecklistDB.addChecklistToDB("Weekly item 3", "Weekly", ChecklistDB.ARCHIVE);
             accessChecklistDB.addCheckListItemsToDB(3,"Weekly item 2 description1" , 0 , ChecklistDB.ARCHIVE);
-            accessChecklistDB.addCheckListItemsToDB(3,"Weekly item 2 description2" , 0 , ChecklistDB.ARCHIVE);
 
             accessChecklistDB.addChecklistToDB("Biweekly item 1", "Biweekly", ChecklistDB.ARCHIVE);
 
